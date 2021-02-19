a0104778f501bd3dd7b8afe3785d579c1894a267
lushihao <lushihaomb@qq.com>
2021-02-19 10:59:45 +0800

    1

--- service/src/main/java/com/lushihao/service/common/ModelType.java
+++ service/src/main/java/com/lushihao/service/common/ModelType.java
@@ -23,15 +23,20 @@ public class ModelType {
      * 游戏约玩
      */
     public static final String MODEL_PLAY = "03";
+    /**
+     * 快递服务
+     */
+    public static final String MODEL_EXPRESS = "04";
 
-    public static Map modelType = new HashMap();
-    public static Map modelTypeReverse = new HashMap();
+    public static Map<String, String> modelType = new HashMap();
+    public static Map<String, String> modelTypeReverse = new HashMap();
 
     static {
         modelType.put(MODEL_CONFESSIONWALL, "告白墙");
         modelType.put(MODEL_MARKET, "跳蚤市场");
         modelType.put(MODEL_LOST, "失物招领");
         modelType.put(MODEL_PLAY, "游戏约玩");
+        modelType.put(MODEL_EXPRESS, "快递服务");
 
         for (Object key : modelType.keySet()) {
             modelTypeReverse.put(modelType.get((String) key), (String) key);
